3ff5220f023ac301addb7dc7802fcf5b7304698c
xogns4909 <xogns4909@naver.com>
2023-08-23 10:29:31 +0900

    Feat : Input클레스 추가 및 선택 입력 메소득 추가

---
+++ src/main/java/input/Input.java
@@ -0,0 +1,28 @@
+package input;
+
+import validation.SelectValidation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Input {
+
+    public static int select;
+
+    public static String expression = "";
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+    static SelectValidation selectValidation = new SelectValidation();
+
+
+    public void select() throws IOException {
+        System.out.println("1.조회");
+        System.out.println("2.계산");
+        select = Integer.parseInt(bufferedReader.readLine());
+        System.out.println("선택 " + select);
+
+    }
+
+
+}
