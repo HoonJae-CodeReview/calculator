a2d84a9597c0c06f063511bb66904cfbd5132b8a
xogns4909 <xogns4909@naver.com>
2023-08-23 10:30:26 +0900

    Feat : 입력에 대한 예외처리 클래스 추가

---
+++ src/main/java/validation/SelectValidation.java
@@ -0,0 +1,13 @@
+package validation;
+
+public class SelectValidation {
+
+    public boolean checkSelectValue(int select){
+        if(select != 1 || select != 2){
+            System.out.println("잘못된 입력값 입니다.");
+            return false;
+        }
+        return true;
+    }
+
+}
