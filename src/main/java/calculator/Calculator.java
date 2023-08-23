250674d011a9cefbda8a11989b5ff594f529c18e
xogns4909 <xogns4909@naver.com>
2023-08-23 17:11:02 +0900

    Feat : 중위 표현식 후위표현식으로 변경 로직 추가

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,11 +1,39 @@
 package calculator;
 
 import java.util.Stack;
+import java.util.logging.Logger;
 
 public class Calculator {
 
-    Stack operator = new Stack();
+
+    Stack<Character> operator = new Stack();
     String postfix = "";
+    String pattern =  "[+\\-*/]";
+
+
+    public String changeToPostFix(String expression){
+        expression.chars().forEach(c -> {
+            char value = (char)c;
+            int operatorPriority = operatorPriority(value);
+             if(operatorPriority == -1){
+                 postfix += value;
+             }
+             else if(operator.isEmpty()){
+                 operator.add(value);
+             }
+             else {
+                 while(!operator.isEmpty() && operator.peek() >= operatorPriority){
+                     postfix += operator.pop();
+                 }
+                 operator.add(value);
+             }
+        });
+        while(!operator.isEmpty()){
+            postfix += operator.pop();
+        }
+        return postfix;
+        }
+
 
 
     public int operatorPriority(char operator){
