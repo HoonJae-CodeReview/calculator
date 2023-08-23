3dace13467f0c8dd7d016addfd65117d6a946671
xogns4909 <xogns4909@naver.com>
2023-08-23 10:33:45 +0900

    Feat : 저장 클래스 추가 및 저장 메소드 추가

---
+++ src/main/java/repository/Repository.java
@@ -0,0 +1,14 @@
+package repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Repository {
+
+    static List<String> result = new ArrayList<>();
+
+    public void store(String expression){
+        result.add(expression);
+    }
+
+}
