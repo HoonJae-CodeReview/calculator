e3d52f69aaaee32580fcee5acf5be8f737cd2a12
SJ70 <seojae70@gmail.com>
2023-08-24 15:33:41 +0900

    Feat: 메뉴 항목의 제목과 동작을 저장하는 클래스 MenuItem

---
+++ src/main/MenuItem.java
@@ -0,0 +1,11 @@
+package src.main;
+
+public class MenuItem {
+    String title;
+    Operation operation;
+
+    public MenuItem(String title, Operation operation) {
+        this.title = title;
+        this.operation = operation;
+    }
+}
