da609f1c7ce128e74e06b81464cef98798465dc9
xogns4909 <xogns4909@naver.com>
2023-08-25 17:16:59 +0900

    Fix : 식 예외처리 버그 수정

--- src/main/java/validation/ExpressionInputValidation.java
+++ src/main/java/validation/ExpressionInputValidation.java
@@ -5,7 +5,7 @@ public class ExpressionInputValidation {
     private final String REGEX = "^\\d+(?: \\+ \\d+| - \\d+| \\* \\d+| \\/ \\d+)*$";
 
     public boolean checkExpressionValue(String expression){
-        if(!REGEX.matches(expression)){
+        if(!expression.matches(REGEX)){
             System.out.println("잘못된 형식의 식입니다.");
             return  false;
         }
