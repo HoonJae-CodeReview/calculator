fd4830663f06ce24b9a119e53f0c7d67bd30d7a4
xogns4909 <xogns4909@naver.com>
2023-08-25 13:28:37 +0900

    Feat  : 후위 연산식 계산 로직 추가

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -35,7 +35,32 @@ public class Calculator {
         }
         return postfix;
         }
-
+    public int calculator(String postfixExpression){
+        Stack<Integer> result = new Stack<>();
+        String[] splitPostfixExpression = postfixExpression.split(" ");
+        Arrays.stream(splitPostfixExpression).forEach(str -> {
+            String value = str;
+            switch (value){
+                case "+":
+                    result.push(result.pop() + result.pop());
+                    break;
+                case "-":
+                    result.push(result.pop() - result.pop());
+                    break;
+                case "*":
+                    result.push(result.pop() * result.pop());
+                    break;
+                case "/":
+                    result.push(result.pop() / result.pop());
+                    break;
+                case " ":
+                    break;
+                default:
+                    result.push(Integer.parseInt(value));
+            }
+        });
+    return result.pop();
+    }
 
 
 
