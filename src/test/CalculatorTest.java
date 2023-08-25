6c9ec7b4ade0d97ed21b933bc5d9b3e84596ed78
SJ70 <seojae70@gmail.com>
2023-08-25 21:53:07 +0900

    Feat: 계산 테스트 문자열 받고 계산하기

--- src/test/CalculatorTest.java
+++ src/test/CalculatorTest.java
@@ -11,6 +11,18 @@ public class CalculatorTest {
     Calculator calculator = new Calculator();
 
     @Test
+    public void testCalculate(){
+        long value1 = calculator.calculate("1 + 5 * 4 * 7 - 8 + 5 + 9 - 5 + 5 * 7 - 5 + 7 - 4 - 4 * 4");
+        Assertions.assertEquals(value1, 1 + 5 * 4 * 7 - 8 + 5 + 9 - 5 + 5 * 7 - 5 + 7 - 4 - 4 * 4);
+
+        long value2 = calculator.calculate("5 * 8 - 9 * 7 / 2 * 5 + 5 + 8 * 5 / 8 * 8 * 8 - 9 + 5 + 5");
+        Assertions.assertEquals(value2, 5 * 8 - 9 * 7 / 2 * 5 + 5 + 8 * 5 / 8 * 8 * 8 - 9 + 5 + 5);
+
+        long value3 = calculator.calculate("5 + 9 - 5 * 7 - 1 + 3 / 5 * 5 + 4 - 5 / 1 - 5 / 4 * 2 - 8");
+        Assertions.assertEquals(value3, 5 + 9 - 5 * 7 - 1 + 3 / 5 * 5 + 4 - 5 / 1 - 5 / 4 * 2 - 8);
+    }
+
+    @Test
     public void testCalculateByOperator(){
         for(long i=-17; i<=17; i+=2){
             for(long j=-17; j<=17; j+=2){
