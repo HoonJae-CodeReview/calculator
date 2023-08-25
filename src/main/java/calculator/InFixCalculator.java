8066eb2eec81fd221004ad21ea3eb20225f41347
xogns4909 <xogns4909@naver.com>
2023-08-25 16:01:38 +0900

    Feat : 중위표기식 계산 클래스 추가

---
+++ src/main/java/calculator/InFixCalculator.java
@@ -0,0 +1,57 @@
+package calculator;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class InFixCalculator implements Calculator{
+
+    private static String postfix = "";
+
+
+    @Override
+    public int calculator(String inFixCalculator) {
+        String postFix = changeToPostFix(inFixCalculator);
+        PostFixCalculator postFixCalculator = new PostFixCalculator();
+        int result = postFixCalculator.calculator(postFix);
+        return  result;
+    }
+
+    public String changeToPostFix(String expression){
+        Stack<String> operator = new Stack();
+
+        String[] splitExpression = expression.split(" ");
+        Arrays.stream(splitExpression).forEach(str -> {
+            String value = str;
+            int operatorPriority = operatorPriority(value);
+            if(operatorPriority == -1){
+                postfix += value+" ";
+            }
+            else if(operator.isEmpty()){
+                operator.add((value+" "));
+            }
+            else {
+                while(!operator.isEmpty() && operatorPriority(operator.peek().substring(0,1)) >= operatorPriority){
+                    postfix += operator.pop();
+                }
+                operator.add((value+" "));
+            }
+        });
+        while(!operator.isEmpty()){
+            postfix += operator.pop();
+        }
+        return postfix;
+    }
+
+
+    public int operatorPriority(String operator){
+        switch (operator){
+            case "+":
+            case "-":
+                return 1;
+            case "*":
+            case "/":
+                return 2;
+        }
+        return -1;
+    }
+}
