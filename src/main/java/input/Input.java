7551509170b1a7b0c4dea6f2e7a55d47b5874518
xogns4909 <xogns4909@naver.com>
2023-08-25 15:00:53 +0900

    Feat : input 인터페이스 추가

--- src/main/java/input/Input.java
+++ src/main/java/input/Input.java
@@ -1,34 +1,10 @@
 package input;
 
-import validation.SelectValidation;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Input {
+public interface Input<T> {
 
-    public static int select;
-
-    public static String expression = "";
-    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-
-    static SelectValidation selectValidation = new SelectValidation();
-
-
-    public int select() throws IOException {
-        System.out.println("1.조회");
-        System.out.println("2.계산");
-        select = Integer.parseInt(bufferedReader.readLine());
-        System.out.println("선택 " + select);
-        System.out.println();
-        return select;
-    }
-
-    public String expression() throws  IOException{
-        expression = bufferedReader.readLine();
-        return expression;
-
-    }
-
+    T input() throws  IOException;
 }
