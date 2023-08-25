9665fcc97f89e0c46c84e1146676be1a26cd12ea
SJ70 <seojae70@gmail.com>
2023-08-25 18:16:52 +0900

    Feat: 올바르지 않은 계산식 예외 처리

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -1,20 +1,26 @@
 package src.main;
 
+import src.main.exception.BadExpressionException;
+
 import java.util.*;
 
 public class Calculator {
 
-    public long calculate(String expression){
+    public long calculate(String expression) throws BadExpressionException {
         StringTokenizer stringTokenizer = new StringTokenizer(expression);
+
+        if(stringTokenizer.countTokens()%2==0){
+            throw new BadExpressionException();
+        }
 
         long resultValue = 0;
 
-        long currentValue = Long.parseLong(stringTokenizer.nextToken());
+        long currentValue = getLongValue(stringTokenizer.nextToken());
         char currentOperator = '+';
 
         while(stringTokenizer.hasMoreTokens()){
-            char nextOperator = stringTokenizer.nextToken().charAt(0);
-            long nextValue = Long.parseLong(stringTokenizer.nextToken());
+            char nextOperator = getOperator(stringTokenizer.nextToken());
+            long nextValue = getLongValue(stringTokenizer.nextToken());
 
 //            System.out.printf("%s%d  |  %d %s %d %s %d\n",nextOperator,nextValue,resultValue,currentOperator,currentValue,nextOperator,nextValue);
 
@@ -38,13 +44,30 @@ public class Calculator {
         return resultValue;
     }
 
-    public long calculateByOperator(long value1, long value2, char Operator){
+    public long calculateByOperator(long value1, long value2, char Operator) throws BadExpressionException {
         switch(Operator){
             case '+' : return value1 + value2;
             case '-' : return value1 - value2;
             case '*' : return value1 * value2;
             case '/' : return value1 / value2;
-            default: return Long.MIN_VALUE;
+            default: throw new BadExpressionException();
+        }
+    }
+
+    public char getOperator(String string) {
+        char character = string.charAt(0);
+        if(string.length()!=1 && character!='+' && character!='-' && character!='*' && character!='/') {
+            throw new BadExpressionException();
+        }
+        return character;
+    }
+
+    public long getLongValue(String string) {
+        try{
+            return Long.parseLong(string);
+        }
+        catch(NumberFormatException e){
+            throw new BadExpressionException();
         }
     }
 
--- src/main/exception/BadExpressionException.java
+++ src/main/exception/BadExpressionException.java
@@ -1,7 +1,10 @@
 package src.main.exception;
 
 public class BadExpressionException extends RuntimeException{
+    public BadExpressionException(){
+        super("올바른 계산식을 입력해주세요");
+    }
     public BadExpressionException(String msg){
-        super(msg);
+        super("올바른 계산식을 입력해주세요 : " + msg);
     }
 }
