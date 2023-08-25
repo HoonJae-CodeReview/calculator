164cf9a25f2ebc1de584f81c43c1cb819bcb30b3
xogns4909 <xogns4909@naver.com>
2023-08-25 13:30:42 +0900

    Feat : 게산기 실행 로직 추가

---
+++ src/main/java/main.java
@@ -0,0 +1,32 @@
+import calculator.Calculator;
+import input.Input;
+import inquire.Inquire;
+import options.Options;
+import repository.Repository;
+import validation.SelectValidation;
+
+import java.io.IOException;
+
+public class main {
+
+
+    public static void main(String[] args) throws IOException {
+        while(true){
+            Input input = new Input();
+            input.select();
+            if(Input.select == Options.CHECK.getValue()){
+                Inquire inquire = new Inquire();
+                inquire.printResult();
+            }
+            else if(Input.select == Options.CALCULATE.getValue()){
+                Repository repository = new Repository();
+                Calculator calculator = new Calculator();
+                String expression = input.expression();
+                String postfixExpression = calculator.changeToPostFix(expression);
+                int result = calculator.calculator(postfixExpression);
+                repository.store(expression + " = " + result);
+
+            }
+        }
+    }
+}
