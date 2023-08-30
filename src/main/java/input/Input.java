e3049bb8475dd45c6bdfe297638f0678a8cf1212
xogns4909 <xogns4909@naver.com>
2023-08-30 17:34:28 +0900

    Refactor: 기존 Input Interface class 로수정

--- src/main/java/input/Input.java
+++ src/main/java/input/Input.java
@@ -1,10 +1,20 @@
 package input;
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public interface Input<T> {
+public class Input {
 
-    T input() throws  IOException;
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    public String selectInput() throws IOException {
+        String selectInput = bufferedReader.readLine();
+        return selectInput;
+    }
+
+    public String expressionInput() throws  IOException {
+        String expressionInput = bufferedReader.readLine();
+        return expressionInput;
+    }
 }
