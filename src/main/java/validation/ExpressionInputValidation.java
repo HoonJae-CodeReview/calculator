f7565d36d11ab994888c978016b0574a92ad1b36
xogns4909 <xogns4909@naver.com>
2023-08-31 23:13:40 +0900

    Refactor : Pattern 클래스를 이용한 정규식 검증으로 변경

--- src/main/java/validation/ExpressionInputValidation.java
+++ src/main/java/validation/ExpressionInputValidation.java
@@ -1,11 +1,15 @@
 package validation;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ExpressionInputValidation {
 
-    private final String REGEX = "^\\d+(?: \\+ \\d+| - \\d+| \\* \\d+| \\/ \\d+)*$";
+    private final Pattern REGEX = Pattern.compile("^\\d+(?: \\+ \\d+| - \\d+| \\* \\d+| \\/ \\d+)*$");
 
     public boolean checkExpressionValue(String expression){
-        if(!expression.matches(REGEX)){
+        Matcher matcher = REGEX.matcher(expression);
+        if(!matcher.matches()){
             System.out.println("잘못된 형식의 식입니다.");
             return  false;
         }
