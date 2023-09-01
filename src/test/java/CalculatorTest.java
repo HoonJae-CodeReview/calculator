7107d5c54d5dce4e86906c4aa73e940d9b434780
xogns4909 <xogns4909@naver.com>
2023-09-01 21:13:23 +0900

    Test : 사칙연산 별 테스트 케이스 추가

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -10,18 +10,48 @@ public class CalculatorTest {
 
 
     @ParameterizedTest
+    @DisplayName("덧셈")
+    @ValueSource(ints = {1,10,100,1000})
+    public void postFixAddCalculate(int expectResult){
+        Accumulator postFixAccumulator = new PostFixAccumulator();
+        int result = postFixAccumulator.calculate("9 1 90 + +");
+        Assertions.assertEquals(expectResult,result);
+    }
+
+    @ParameterizedTest
+    @DisplayName("뺼셈")
+    @ValueSource(ints = {1,10,100,1000})
+    public void postFixMinusCalculate(int expectResult){
+        Accumulator postFixAccumulator = new PostFixAccumulator();
+        int result = postFixAccumulator.calculate("9 8 -");
+        Assertions.assertEquals(expectResult,result);
+    }
+
+    @ParameterizedTest
+    @DisplayName("곱셈")
+    @ValueSource(ints = {1,10,100,1000})
+    public void postFixMultiplyCalculate(int expectResult){
+        Accumulator postFixAccumulator = new PostFixAccumulator();
+        int result = postFixAccumulator.calculate("10 100 *");
+        Assertions.assertEquals(expectResult,result);
+    }
+
+    @ParameterizedTest
+    @DisplayName("나눗셈")
+    @ValueSource(ints = {1,10,100,1000})
+    public void postFixDivideCalculate(int expectResult){
+        Accumulator postFixAccumulator = new PostFixAccumulator();
+        int result = postFixAccumulator.calculate("100 10 /");
+        Assertions.assertEquals(expectResult,result);
+    }
+
+    @ParameterizedTest
     @DisplayName("사칙연산")
     @ValueSource(ints = {10,20,27})
-    public void PostFixCalculator(int expectResult) {
+    public void PostFixCalculate(int expectResult) {
         PostFixAccumulator calculator = new PostFixAccumulator();
         int result = calculator.calculate("3 4 5 + *");
         Assertions.assertEquals(expectResult,result);
     }
 
-    @Test
-    public void InFixCalculator(){
-        Accumulator calculator = new PostFixAccumulator();
-        int expect = calculator.calculate("3 + 2 + 5 * 2 / 1 ");
-        Assertions.assertEquals(15,expect);
-    }
 }
