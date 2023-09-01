8152b7f8e8d8bb70f49edcf0de5ae3e0bf769dfb
xogns4909 <xogns4909@naver.com>
2023-09-02 00:23:08 +0900

    Test : 인메모리 저장 테스트 추가

---
+++ src/test/java/RepositoryTest.java
@@ -0,0 +1,26 @@
+import java.util.LinkedHashMap;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import repository.Repository;
+
+public class RepositoryTest {
+  private Repository repository;
+
+  @BeforeEach
+  public void setup() {
+    repository = new Repository();
+  }
+
+  @ParameterizedTest
+  @CsvSource({"2 + 3, 5", "4 * 5, 20", "10 / 2, 5"})
+  public void testStoreAndGetResult(String expression, String result) {
+
+    repository.store(expression, result);
+    LinkedHashMap<String, String> resultMap = repository.getResult();
+
+    Assertions.assertEquals(result, resultMap.get(expression));
+  }
+}
+
