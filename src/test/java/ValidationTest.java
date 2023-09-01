5ae1ab2a073265f6b7f0c8b2de90b75f408526be
xogns4909 <xogns4909@naver.com>
2023-09-01 21:39:31 +0900

    Test : 옵션 입력 검증 테스트

--- src/test/java/ValidationTest.java
+++ src/test/java/ValidationTest.java
@@ -1,3 +1,5 @@
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -14,4 +16,17 @@ public class ValidationTest {
     boolean result = selectValidation.checkSelectValue(selectInput);
     Assertions.assertTrue(result);
   }
+
+  @ParameterizedTest
+  @DisplayName("옵션 선택 검증 테스트")
+  @ValueSource(strings = {"1","2","10000"})
+  public void OptionSelectValidationTest(String selectInput){
+    ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(outputStreamCaptor));
+    SelectValidation selectValidation = new SelectValidation();
+    selectValidation.checkSelectValue(selectInput);
+    Assertions.assertTrue(outputStreamCaptor.toString().trim().equals("잘못된 입력값 입니다."));
+  }
+
+
 }
