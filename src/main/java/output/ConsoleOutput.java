a157b8b4fed511122afd53a37da398125111e38e
xogns4909 <xogns4909@naver.com>
2023-09-01 13:21:18 +0900

    Fix : 조회로직 수정

--- src/main/java/output/ConsoleOutput.java
+++ src/main/java/output/ConsoleOutput.java
@@ -2,19 +2,27 @@ package output;
 
 import repository.Repository;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ConsoleOutput implements Output {
 
     @Override
-    public void printOperationResult(){
-        List<String> result = repository.getResult();
-        result.forEach(System.out::println);
+    public void printOperationResult() {
+        LinkedHashMap<String, String> result = repository.getResult();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Map.Entry<String, String> entry : result.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            stringBuilder.append(key).append(" = ").append(value).append("\n");
+        }
+        System.out.println(stringBuilder);
     }
 
     @Override
     public void print(String msg) {
-
+        System.out.println(msg);
     }
 
 
