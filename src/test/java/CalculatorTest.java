a5bfd088dc05fa8fb133fad36423fd0ef9eb3ceb
xogns4909 <xogns4909@naver.com>
2023-09-02 00:15:47 +0900

    Test : @CsvSource를 이용한 테스트로 수정

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class CalculatorTest {
@@ -11,46 +13,46 @@ public class CalculatorTest {
 
   @ParameterizedTest
   @DisplayName("덧셈")
-  @ValueSource(ints = {1, 10, 100, 1000})
-  public void postFixAddCalculate(int expectResult) {
+  @CsvSource(value = {"'1 2 + 3 +', 6", "'10 20 + 30 +', 60", "'100 200 + 300 +', 600"})
+  public void postFixAddCalculate(String expression,int expectResult) {
     Accumulator postFixAccumulator = new PostFixAccumulator();
-    int result = postFixAccumulator.calculate("9 1 90 + +");
+    int result = postFixAccumulator.calculate(expression);
     Assertions.assertEquals(expectResult, result);
   }
 
   @ParameterizedTest
   @DisplayName("뺼셈")
-  @ValueSource(ints = {1, 10, 100, 1000})
-  public void postFixMinusCalculate(int expectResult) {
+  @CsvSource(value = {"'1 2 - 3 -', -4", "'10 30 - 20 -', -40", "'300 200 - 1 -', 99"})
+  public void postFixMinusCalculate(String expression,int expectResult) {
     Accumulator postFixAccumulator = new PostFixAccumulator();
-    int result = postFixAccumulator.calculate("9 8 -");
+    int result = postFixAccumulator.calculate(expression);
     Assertions.assertEquals(expectResult, result);
   }
 
   @ParameterizedTest
   @DisplayName("곱셈")
-  @ValueSource(ints = {1, 10, 100, 1000})
-  public void postFixMultiplyCalculate(int expectResult) {
+  @CsvSource(value = {"'1 2 * 3 *', 6", "'10 20 * 30 *', 6000", "'100 200 * 300 * ', 6000000"})
+  public void postFixMultiplyCalculate(String expression,int expectResult) {
     Accumulator postFixAccumulator = new PostFixAccumulator();
-    int result = postFixAccumulator.calculate("10 100 *");
+    int result = postFixAccumulator.calculate(expression);
     Assertions.assertEquals(expectResult, result);
   }
 
   @ParameterizedTest
   @DisplayName("나눗셈")
-  @ValueSource(ints = {1, 10, 100, 1000})
-  public void postFixDivideCalculate(int expectResult) {
+  @CsvSource(value = {"'100 10 / 1 /', 10", "'10 2 /', 5", "'10000 20 / 10 /', 50"})
+  public void postFixDivideCalculate(String expression,int expectResult) {
     Accumulator postFixAccumulator = new PostFixAccumulator();
-    int result = postFixAccumulator.calculate("100 10 /");
+    int result = postFixAccumulator.calculate(expression);
     Assertions.assertEquals(expectResult, result);
   }
 
   @ParameterizedTest
   @DisplayName("사칙연산")
-  @ValueSource(ints = {10, 20, 27})
-  public void PostFixCalculate(int expectResult) {
+  @CsvSource(value = {"'5 3 2 * + 8 4 / -', 9", "'7 4 * 2 / 3 + 1 -', 16", "'9 5 - 2 * 6 3 / +', 10"})
+  public void PostFixCalculate(String expression,int expectResult) {
     PostFixAccumulator calculator = new PostFixAccumulator();
-    int result = calculator.calculate("3 4 5 + *");
+    int result = calculator.calculate(expression);
     Assertions.assertEquals(expectResult, result);
   }
 
--- src/test/java/ChangToPostfixTest.java
+++ src/test/java/ChangToPostfixTest.java
@@ -1,20 +1,19 @@
 import convertor.InfixToPostfixConverter;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class ChangToPostfixTest {
 
   @ParameterizedTest
   @DisplayName("중위 표기식 후위 표기식 변환")
-  @ValueSource(strings = {"1 2 3 * + ", "1 + 2 * 3"})
-  public void InfixToPostfixTest(String expectResult) {
-    String input = "1 + 2 * 3";
+  @CsvSource(value = {"'7 * 4 / 2 + 3 - 1','7 4 * 2 / 3 + 1 - ' ",
+      "'9 - 5 * 2 + 6 / 3', '9 5 2 * - 6 3 / + '"})
+  public void InfixToPostfixTest(String infixExpression, String postFinExpression) {
     InfixToPostfixConverter calculator = new InfixToPostfixConverter();
-    String value = calculator.changeToPostFix(input);
-    Assertions.assertEquals(expectResult, value);
+    String result = calculator.changeToPostFix(infixExpression);
+    Assertions.assertEquals(postFinExpression, result);
   }
 
 }
