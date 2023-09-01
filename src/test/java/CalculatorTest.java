123488deba3bd84c56c8901d1a4beede25adbc92
xogns4909 <xogns4909@naver.com>
2023-09-01 21:21:17 +0900

    Test : 중위표기식 후위표기식 변환 테스트 케이스 추가

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -9,49 +9,49 @@ import org.junit.jupiter.params.provider.ValueSource;
 public class CalculatorTest {
 
 
-    @ParameterizedTest
-    @DisplayName("덧셈")
-    @ValueSource(ints = {1,10,100,1000})
-    public void postFixAddCalculate(int expectResult){
-        Accumulator postFixAccumulator = new PostFixAccumulator();
-        int result = postFixAccumulator.calculate("9 1 90 + +");
-        Assertions.assertEquals(expectResult,result);
-    }
+  @ParameterizedTest
+  @DisplayName("덧셈")
+  @ValueSource(ints = {1, 10, 100, 1000})
+  public void postFixAddCalculate(int expectResult) {
+    Accumulator postFixAccumulator = new PostFixAccumulator();
+    int result = postFixAccumulator.calculate("9 1 90 + +");
+    Assertions.assertEquals(expectResult, result);
+  }
 
-    @ParameterizedTest
-    @DisplayName("뺼셈")
-    @ValueSource(ints = {1,10,100,1000})
-    public void postFixMinusCalculate(int expectResult){
-        Accumulator postFixAccumulator = new PostFixAccumulator();
-        int result = postFixAccumulator.calculate("9 8 -");
-        Assertions.assertEquals(expectResult,result);
-    }
+  @ParameterizedTest
+  @DisplayName("뺼셈")
+  @ValueSource(ints = {1, 10, 100, 1000})
+  public void postFixMinusCalculate(int expectResult) {
+    Accumulator postFixAccumulator = new PostFixAccumulator();
+    int result = postFixAccumulator.calculate("9 8 -");
+    Assertions.assertEquals(expectResult, result);
+  }
 
-    @ParameterizedTest
-    @DisplayName("곱셈")
-    @ValueSource(ints = {1,10,100,1000})
-    public void postFixMultiplyCalculate(int expectResult){
-        Accumulator postFixAccumulator = new PostFixAccumulator();
-        int result = postFixAccumulator.calculate("10 100 *");
-        Assertions.assertEquals(expectResult,result);
-    }
+  @ParameterizedTest
+  @DisplayName("곱셈")
+  @ValueSource(ints = {1, 10, 100, 1000})
+  public void postFixMultiplyCalculate(int expectResult) {
+    Accumulator postFixAccumulator = new PostFixAccumulator();
+    int result = postFixAccumulator.calculate("10 100 *");
+    Assertions.assertEquals(expectResult, result);
+  }
 
-    @ParameterizedTest
-    @DisplayName("나눗셈")
-    @ValueSource(ints = {1,10,100,1000})
-    public void postFixDivideCalculate(int expectResult){
-        Accumulator postFixAccumulator = new PostFixAccumulator();
-        int result = postFixAccumulator.calculate("100 10 /");
-        Assertions.assertEquals(expectResult,result);
-    }
+  @ParameterizedTest
+  @DisplayName("나눗셈")
+  @ValueSource(ints = {1, 10, 100, 1000})
+  public void postFixDivideCalculate(int expectResult) {
+    Accumulator postFixAccumulator = new PostFixAccumulator();
+    int result = postFixAccumulator.calculate("100 10 /");
+    Assertions.assertEquals(expectResult, result);
+  }
 
-    @ParameterizedTest
-    @DisplayName("사칙연산")
-    @ValueSource(ints = {10,20,27})
-    public void PostFixCalculate(int expectResult) {
-        PostFixAccumulator calculator = new PostFixAccumulator();
-        int result = calculator.calculate("3 4 5 + *");
-        Assertions.assertEquals(expectResult,result);
-    }
+  @ParameterizedTest
+  @DisplayName("사칙연산")
+  @ValueSource(ints = {10, 20, 27})
+  public void PostFixCalculate(int expectResult) {
+    PostFixAccumulator calculator = new PostFixAccumulator();
+    int result = calculator.calculate("3 4 5 + *");
+    Assertions.assertEquals(expectResult, result);
+  }
 
 }
--- src/test/java/ChangToPostfixTest.java
+++ src/test/java/ChangToPostfixTest.java
@@ -1,15 +1,20 @@
 import convertor.InfixToPostfixConverter;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class ChangToPostfixTest {
 
-@Test
-public void PostfixTest(){
+  @ParameterizedTest
+  @DisplayName("중위 표기식 후위 표기식 변환")
+  @ValueSource(strings = {"1 2 3 * + ", "1 + 2 * 3"})
+  public void InfixToPostfixTest(String expectResult) {
     String input = "1 + 2 * 3";
-    String expect = "1 2 3 * + ";
     InfixToPostfixConverter calculator = new InfixToPostfixConverter();
     String value = calculator.changeToPostFix(input);
-    Assertions.assertEquals(expect,value);
-}
+    Assertions.assertEquals(expectResult, value);
+  }
+
 }
