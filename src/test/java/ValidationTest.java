cfddb59e41fe3788fdfc6829297d6438301ee471
xogns4909 <xogns4909@naver.com>
2023-09-02 00:15:21 +0900

    Test : 테스트 데이터 변경

--- src/test/java/ValidationTest.java
+++ src/test/java/ValidationTest.java
@@ -11,7 +11,7 @@ public class ValidationTest {
 
   @ParameterizedTest
   @DisplayName("옵션 선택 테스트")
-  @ValueSource(strings = {"1", "2", "10000"})
+  @ValueSource(strings = {"1", "2"})
   public void OptionSelectTest(String selectInput) {
     SelectValidation selectValidation = new SelectValidation();
     boolean result = selectValidation.checkSelectValue(selectInput);
@@ -20,7 +20,7 @@ public class ValidationTest {
 
   @ParameterizedTest
   @DisplayName("옵션 선택 검증 테스트")
-  @ValueSource(strings = {"1", "2", "10000"})
+  @ValueSource(strings = {"10000","abc","!!@@##"})
   public void OptionSelectValidationTest(String selectInput) {
     ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
     System.setOut(new PrintStream(outputStreamCaptor));
@@ -31,7 +31,7 @@ public class ValidationTest {
 
   @ParameterizedTest
   @DisplayName("식 입력 테스트")
-  @ValueSource(strings = {"3 + 1 + 2", "1 * 2 / 100 + 2 - 10", "!!@@##"})
+  @ValueSource(strings = {"3 + 1 + 2", "1 * 2 / 100 + 2 - 10"})
   public void inputExpressionTest(String expressionInput) {
     ExpressionInputValidation expressionInputValidation = new ExpressionInputValidation();
     boolean result = expressionInputValidation.checkExpressionValue(expressionInput);
@@ -40,7 +40,7 @@ public class ValidationTest {
 
   @ParameterizedTest
   @DisplayName("식 입력 검증 테스트")
-  @ValueSource(strings = {"3 + 1 + 2", "1 * 2 / 100 + 2 - 10", "!!@@##"})
+  @ValueSource(strings = {"1*2/100+2-10", "!!@@##"})
   public void inputExpressionValidationTest(String expressionInput) {
     ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
     System.setOut(new PrintStream(outputStreamCaptor));
