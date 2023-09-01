2410415019c142552844f956860446bcfef45fbe
xogns4909 <xogns4909@naver.com>
2023-09-01 13:01:26 +0900

    Refactor : Output 을 이용한 출력로직으로 변경

--- src/main/java/validation/ExpressionInputValidation.java
+++ src/main/java/validation/ExpressionInputValidation.java
@@ -1,16 +1,20 @@
 package validation;
 
+import output.ConsoleOutput;
+import output.Output;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ExpressionInputValidation {
 
+    private final Output output = new ConsoleOutput();
     private final Pattern REGEX = Pattern.compile("^\\d+(?: \\+ \\d+| - \\d+| \\* \\d+| \\/ \\d+)*$");
 
     public boolean checkExpressionValue(String expression){
         Matcher matcher = REGEX.matcher(expression);
         if(!matcher.matches()){
-            System.out.println("잘못된 형식의 식입니다.");
+            output.print("잘못된 형식의 식입니다.");
             return  false;
         }
         return true;
--- src/main/java/validation/SelectValidation.java
+++ src/main/java/validation/SelectValidation.java
@@ -1,15 +1,19 @@
 package validation;
 
+import output.ConsoleOutput;
+import output.Output;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class SelectValidation {
 
-    private static final Pattern REGEX  = Pattern.compile("^[12]+$");
+    private final Output output = new ConsoleOutput();
+    private final Pattern REGEX  = Pattern.compile("^[12]+$");
     public boolean checkSelectValue(String select){
         Matcher matcher = REGEX.matcher(select);
         if (!matcher.matches()){
-            System.out.println("잘못된 입력값 입니다.");
+            output.print("잘못된 입력값 입니다.");
             return false;
         }
         return true;
