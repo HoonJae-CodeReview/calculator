f39924972a984b00277a32ed4c54d454991f91d5
xogns4909 <xogns4909@naver.com>
2023-09-02 00:35:35 +0900

    Test : Repository 테스트 시 생성

--- src/test/java/RepositoryTest.java
+++ src/test/java/RepositoryTest.java
@@ -8,19 +8,16 @@ import repository.Repository;
 public class RepositoryTest {
   private Repository repository;
 
-  @BeforeEach
-  public void setup() {
-    repository = new Repository();
-  }
 
   @ParameterizedTest
   @CsvSource({"2 + 3, 5", "4 * 5, 20", "10 / 2, 5"})
   public void testStoreAndGetResult(String expression, String result) {
-
+    repository = new Repository();
     repository.store(expression, result);
     LinkedHashMap<String, String> resultMap = repository.getResult();
 
     Assertions.assertEquals(result, resultMap.get(expression));
   }
+
 }
 
