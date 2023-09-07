751994a960b790053b346129ddf79a8ff2443b94
SJ70 <seojae70@gmail.com>
2023-09-07 13:04:47 +0900

    Refactor: ExpressionTrimmer 생성자에 private 키워드 명시 -> 생성하지 못하도록 함

--- src/main/ExpressionTrimmer.java
+++ src/main/ExpressionTrimmer.java
@@ -4,6 +4,8 @@ import java.util.StringTokenizer;
 
 public final class ExpressionTrimmer {
 
+    private ExpressionTrimmer() {}
+
     public static String trimExpression(String expression){
         StringTokenizer stringTokenizer = new StringTokenizer(expression);
         StringBuilder stringBuilder = new StringBuilder();
